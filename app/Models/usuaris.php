9bc2cd6e178179d75c900671d908d00146cb8c83
Joaquín <joaquinmscollo@gmail.com>
2026-03-17 18:02:23 +0100

    Progreso

--- app/Models/usuaris.php
+++ app/Models/usuaris.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class usuaris extends Model
 {
     //
+    protected $table = 'usuaris';
+    protected $fillable = ['correu', 'contrasenya', 'nom', 'cognoms', 'telefon'];
+
+    public function 
 }
