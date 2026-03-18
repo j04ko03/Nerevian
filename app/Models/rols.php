5200b6f99af862bf5e3c5585b80548d0e0dcce06
Joaquín <joaquinmscollo@gmail.com>
2026-03-18 16:00:15 +0100

    Relaciones de usuaris y rols hecha

--- app/Models/rols.php
+++ app/Models/rols.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class rols extends Model
 {
     //
+
+    public function usuaris(): HasOne
+    {
+        return $this->hasOne(usuaris::class , 'rol_id');
+    }
 }
--- app/Models/usuaris.php
+++ app/Models/usuaris.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class usuaris extends Model
 {
@@ -10,5 +12,27 @@ class usuaris extends Model
     protected $table = 'usuaris';
     protected $fillable = ['correu', 'contrasenya', 'nom', 'cognoms', 'telefon'];
 
-    public function 
+    public function peticions_registre(): HasOne
+    {
+        return $this->hasOne(peticions_registre::class , 'resolt_per');
+    }
+    public function clients(): HasOne
+    {
+        return $this->hasOne(clients::class , 'usuari_id');
+    }
+
+    public function solicitud(): HasMany
+    {
+        return $this->hasMany(solicitud::class , 'operador_id');
+    }
+
+    public function documents(): HasMany
+    {
+        return $this->hasMany(documents::class , 'pujat_per');
+    }
+
+    public function operacions(): HasMany
+    {
+        return $this->hasMany(operacions::class , 'operador_id');
+    }
 }
