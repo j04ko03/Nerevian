b1627ed69d42667f02556a24914109ad407e290a
Ivory Dipasupil <86897402+ivorydd@users.noreply.github.com>
2026-04-03 18:05:47 +0200

    Model estats_solicituds created
    States introduced to DB
    1 > nueva
    2 > en_revision
    3 > cotizada
    4 > en_negociacion
    5 > rechazada

--- app/Models/estats_solicituds.php
+++ app/Models/estats_solicituds.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class estats_solicituds extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'estats_solicituds';
+    protected $primaryKey = 'id_estat';
+    protected $fillable = [
+        'estat',
+    ];
+    public $timestamps = false;
 }
