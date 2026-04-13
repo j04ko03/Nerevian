ee758041658359b5138723499165f5e9793998ea
Joaquín <joaquinmscollo@gmail.com>
2026-04-13 19:48:53 +0200

    Arreglo 4

--- config/database.php
+++ config/database.php
@@ -114,10 +114,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'options' => [
-                PDO::SQLSRV_ATTR_FETCHES_NUMERIC_TYPE => true,
-                'TrustServerCertificate' => true,
-            ],
+            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
+            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
     ],
