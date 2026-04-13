0156f66c83fa3d413b8d7a0d73f2b2066f41afd4
Joaquín <joaquinmscollo@gmail.com>
2026-04-13 23:16:39 +0200

    Arreglo 5

--- config/database.php
+++ config/database.php
@@ -114,8 +114,8 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'false'), // Quita las // y asegúrate que sea 'false'
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'), // Quita las // y que sea 'true'
         ],
 
     ],
